c20e5ac6bc286b9d2d0123a7188eac4aeef5e3bb
Tenergy Innovation <tenergy.innovation@gmail.com>
2019-03-25 23:00:51 +0700

    Arduino_UNO_PIR.ino
    Arduino_UNO_PIR

---
+++ Code/Arduino_UNO_PIR/Arduino_UNO_PIR.ino
@@ -0,0 +1,57 @@
+/***********************************************************************
+ * Project      :     Arduino_UNO_PIR
+ * Description  :     Motion Sensor Test           
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     14 Nov 2018
+ * Revision     :     1.0
+ * Rev1.0       :     Original
+ * Facebook     :     https://www.facebook.com/tenergy.innovation
+ * Email        :     tenergy.innovation@gmail.com
+ * TEL          :     +6689-140-7205
+ ***********************************************************************/
+
+/**************************************/
+/*         GPIO Define                */
+/**************************************/  
+#define  inputPin     7
+
+ /***********************************************************************
+ * FUNCTION:    setup
+ * DESCRIPTION: setup process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void setup(){
+
+  pinMode(inputPin, INPUT);
+  Serial.begin(9600);
+  Serial.println("************************************************");
+  Serial.println("Project      :  Arduino_UNO_PIR");                 
+  Serial.println("Author       :  Tenergy Innovation Co., Ltd.");
+  Serial.println("Date         :  14 Nov 2018");
+  Serial.println("Facebook     :  https://www.facebook.com/tenergy.innovation");
+  Serial.println("Email        :  tenergy.innovation@gmail.com");
+  Serial.println("TEL          :  +6689-140-7205");
+  Serial.println("************************************************");
+}
+
+ /***********************************************************************
+ * FUNCTION:    loop
+ * DESCRIPTION: loop process
+ * PARAMETERS:  nothing
+ * RETURNED:    nothing
+ ***********************************************************************/
+void loop(){
+  int value= digitalRead(inputPin);
+
+  if (value == HIGH)
+  {
+    Serial.println("High");
+  }
+
+  else
+  {
+    Serial.println("LOW");
+  }
+  delay(300);
+}
